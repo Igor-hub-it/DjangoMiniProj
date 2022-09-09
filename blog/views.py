b96c8f8c614040b7844a519f3666cfbbf1ce7de8
Igor <igorpigidin@gmail.com>
2022-09-10 00:32:07 +0700

    add admin settings and model

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(requesst):
+    return HttpResponse('<h1>Hello world!</h1>')
+
+def test(request):
+    return HttpResponse('<h5>its testpage</h5>')
