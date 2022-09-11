39e3f2de465ec5a5e3356b476140c1d04d9554f8
Igor <igorpigidin@gmail.com>
2022-09-11 08:28:40 +0700

    add mediaroot and settings of admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Article
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title')
+    list_display = ('id', 'title', 'created_at')
+    list_display_links = ('id', 'title', 'created_at')
 
 admin.site.register(Article, ArticleAdmin)
+
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
 
 # Create your views here.
 
-def home(requesst):
-    return HttpResponse('<h1>Hello world!</h1>')
+def home(request):
+    posts = Article.objects.all()
+    return render(request, 'blog/home.html', {'posts': posts})
 
 def test(request):
     return HttpResponse('<h5>its testpage</h5>')
